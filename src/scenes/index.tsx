5e050e035c2a6f5ed9e0c78b21b7a8eb57639c44
chn <nikikwiki@mail.ru>
2022-05-13 17:33:40 +0300

    add multiple themes

--- src/scenes/index.tsx
+++ src/scenes/index.tsx
@@ -1,32 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import {
-    blue, blueGrey, grey, indigo, teal
+    blue, grey, indigo
 } from '@mui/material/colors';
+import { httpService } from 'http-service';
 import { Main } from './main';
 
-const theme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: blue,
-        divider: blue[700],
-        background: {
-            default: blue[900],
-            paper: blue[900]
-        },
-        text: {
-            primary: '#fff',
-            secondary: grey[700]
-        }
-    }
-});
-
 export const RootScene: React.FC<any> = (): JSX.Element => {
+    const [ weather, setWeather ] = useState<any>(null);
+    const [ forecast, setForecast ] = useState<any>(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            httpService.getWeather(lat, lon).then(res => {
+                setWeather(res[0].data);
+                setForecast(res[1].data);
+            });
+        }, error => {
+            console.log('Ошибка - ', error);
+        });
+    }, []);
+
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+            primary: indigo,
+            divider: indigo[700],
+            background: {
+                default: indigo[900],
+                paper: indigo[900]
+            },
+            text: {
+                primary: '#fff',
+                secondary: grey[900]
+            }
+        }
+    });
+
+    const lightTheme = createTheme({
+        palette: {
+            mode: 'dark',
+            primary: blue,
+            divider: blue[700],
+            background: {
+                default: blue[900],
+                paper: blue[900]
+            },
+            text: {
+                primary: '#fff',
+                secondary: grey[700]
+            }
+        }
+    });
+
     return (
         <>
-            <ThemeProvider theme={theme}>
-                <Main />
-            </ThemeProvider>
+            {weather && forecast
+            && (
+                <ThemeProvider theme={new Date().getHours() > 7 || new Date().getHours() < 21 ? lightTheme : darkTheme}>
+                    <Main weather={weather} forecast={forecast} />
+                </ThemeProvider>
+            )}
 
         </>
     );
--- src/scenes/main/Main.tsx
+++ src/scenes/main/Main.tsx
@@ -1,32 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
     CentralInfoComponent, Forecast, RegionInfo, Sidebar
 } from 'components';
 import MenuIcon from '@mui/icons-material/Menu';
 import IconButton from '@mui/material/IconButton';
 import { useTheme } from '@mui/material/styles';
-import { httpService } from 'http-service';
 import clsx from 'clsx';
 import styles from './styles.scss';
 
-export const MainComponent = () => {
-    const [ weather, setWeather ] = useState<any>(null);
-    const [ forecast, setForecast ] = useState<any>(null);
+export const MainComponent = ({ weather, forecast }: any) => {
     const [ openSidebar, setOpenSidebar ] = useState<boolean>(true);
     const theme = useTheme();
-
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-            httpService.getWeather(lat, lon).then(res => {
-                setWeather(res[0].data);
-                setForecast(res[1].data);
-            });
-        }, error => {
-            console.log('Ошибка - ', error);
-        });
-    }, []);
 
     const mainStyles = clsx(styles.main, {
         [styles.mainPushed]: openSidebar
@@ -34,35 +18,31 @@ export const MainComponent = () => {
 
     return (
         <>
-            { weather && forecast && (
-                <>
-                    <div
-                        className={mainStyles}
-                        style={{ backgroundColor: theme.palette.primary.main }}
+            <div
+                className={mainStyles}
+                style={{ backgroundColor: theme.palette.primary.main }}
+            >
+                <div className={styles.central}>
+                    <CentralInfoComponent weather={weather} />
+                </div>
+                <RegionInfo weather={weather} />
+                <div className={styles.sidebar}>
+                    <IconButton
+                        onClick={() => setOpenSidebar(true)}
+                        style={openSidebar ? { visibility: 'hidden' } : { visibility: 'visible' }}
                     >
-                        <div className={styles.central}>
-                            <CentralInfoComponent weather={weather} />
-                        </div>
-                        <RegionInfo weather={weather} />
-                        <div className={styles.sidebar}>
-                            <IconButton
-                                onClick={() => setOpenSidebar(true)}
-                                style={openSidebar ? { visibility: 'hidden' } : { visibility: 'visible' }}
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                        </div>
-                        <div className={styles.forecast}>
-                            <Forecast forecast={forecast} />
-                        </div>
-                    </div>
-                    <Sidebar
-                        weather={weather}
-                        openSidebar={openSidebar}
-                        closeSidebar={() => setOpenSidebar(false)}
-                    />
-                </>
-            )}
+                        <MenuIcon />
+                    </IconButton>
+                </div>
+                <div className={styles.forecast}>
+                    <Forecast forecast={forecast} />
+                </div>
+            </div>
+            <Sidebar
+                weather={weather}
+                openSidebar={openSidebar}
+                closeSidebar={() => setOpenSidebar(false)}
+            />
         </>
     );
 };
